28665b222cd961af4f6d0cd656788723050860eb
KIRA-8XY <ariqalwin94004@gmail.com>
2025-06-15 20:09:18 +0700

    chore: import AdminController for route management

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EvakuasiController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/request-evakuasi', [EvakuasiController::class, 'index']);
 Route::post('/request-evakuasi', [EvakuasiController::class, 'store'])->name('evakuasi.store');
