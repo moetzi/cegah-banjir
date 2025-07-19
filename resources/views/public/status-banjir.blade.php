487fa1606ad4337134ff6b07014024eeb9479c9c
Mutiara Noor Fauzia <143862669+moetzi@users.noreply.github.com>
2025-07-19 16:04:39 +0700

    re

--- resources/views/public/status-banjir.blade.php
+++ resources/views/public/status-banjir.blade.php
@@ -154,14 +154,10 @@
 
     <script>
         const firebaseConfig = {
-            apiKey: "AIzaSyCtvJ2hGE0jvp_wOGqH8uG7pg1sgrvTLwo",
-            authDomain: "cegah-banjir.firebaseapp.com",
-            databaseURL: "https://cegah-banjir-default-rtdb.asia-southeast1.firebasedatabase.app",
-            projectId: "cegah-banjir",
-            storageBucket: "cegah-banjir.appspot.com",
-            messagingSenderId: "878634677325",
-            appId: "1:878634677325:web:d6077eaf62ea13490a1804",
-            measurementId: "G-W3YXYXR23B"
+        apiKey: "API_KEY",
+        authDomain: "AUTH_DOMAIN",
+        databaseURL: "DB_URL",
+        projectId: "PROJECT_ID",
         };
         firebase.initializeApp(firebaseConfig);
         const database = firebase.database();
